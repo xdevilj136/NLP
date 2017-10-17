336c8e0b90ad415b983f671e84b4eff9d446adc9
xiaojun <xiaojun@xiaojunMacBook-Pro.local>
2017-10-17 22:07:54 +0800

    数据源配置详情

--- src/page/main/data-source-config/detail.vue
+++ src/page/main/data-source-config/detail.vue
@@ -6,113 +6,41 @@
                 <el-breadcrumb-item>数据详情</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        <div class="clearfix mgt15">
-            <span class="detail-left-label">数据名称:</span>
-            <div class="detail-right-content-box">
-                <el-input v-model="dataSource.name" class="input" placeholder="请输入" size="small"></el-input>
-            </div>
-        </div>
-        <div class="clearfix mgt15">
-            <span class="detail-left-label">数据源格式:</span>
-            <div class="detail-right-content-box">
-                <div>
-                    <el-select v-model="dataSource.type" @change="dataSourceTypeChange" class="input" size="small">
-                        <el-option v-for="item in dataTypeOptions" :key="item.value" :label="item.label" :value="item.value">
-                        </el-option>
-                    </el-select>
-                </div>
-            </div>
-        </div>
-        <div class="clearfix mgt15" style="position:relative;">
-            <span class="detail-left-label">数据源文件:</span>
-            <div class="detail-right-content-box">
-                <el-upload :disabled="isUploading" class="upload-demo" action="/api/inputSource/upload" :before-upload="beforeUpload" :on-progress="uploading" :on-success="uploadSuccess" :on-error="uploadError" :show-file-list="false">
-                    <el-input v-model="uploadDataName" readonly class="input" placeholder="浏览..." size="small"></el-input>
-                </el-upload>
-            </div>
-            <el-progress v-if="isUploading" class="uploading-progress" type="circle" :width="50" :percentage="uploadingPercent" :status="uploadingStatus"></el-progress>
-        </div>
-        <div v-if="targetSheetShow" class="clearfix mgt15">
-            <span class="detail-left-label">目标表:</span>
-            <div class="detail-right-content-box">
-                <el-select :disabled="!dataLoaded" v-model="dataSource.targetSheet" placeholder="- -" class="input" size="small">
-                    <el-option v-for="item in sheetOptions" :key="item.value" :label="item.label" :value="item.value">
-                    </el-option>
-                </el-select>
-            </div>
-        </div>
-        <div v-if="encodeShow" class="clearfix mgt15">
-            <span class="detail-left-label">编码格式:</span>
-            <div class="detail-right-content-box">
-                <el-select :disabled="!dataLoaded" v-model="dataSource.encode" class="input" placeholder="- -" size="small">
-                    <el-option v-for="item in encodeOptions" :key="item.value" :label="item.label" :value="item.value">
-                    </el-option>
-                </el-select>
-            </div>
-        </div>
-        <div v-if="symbolShow" class="clearfix mgt15">
-            <span class="detail-left-label">分隔符:</span>
-            <div class="detail-right-content-box">
-                <el-input @focus="symbolsModalShow" v-model="dataSource.splitSymbols" class="input" placeholder="请选择" size="small" readonly></el-input>
-            </div>
-        </div>
-        <div v-if="topRowIsTitleShow" class="clearfix mgt15">
-            <span class="detail-left-label">首行标题行:</span>
-            <div class="detail-right-content-box mgt10">
-                <el-radio-group v-model="dataSource.topRowIsTitle">
-                    <el-radio :label="true">是</el-radio>
-                    <el-radio :label="false">否</el-radio>
-                </el-radio-group>
-            </div>
-        </div>
-        <div class="clearfix mgt15">
-            <span class="detail-left-label">数据源预览:</span>
-            <div class="detail-right-content-box  mgt10">
-                <el-button v-if="!textIsOnPreview&&!tableIsOnPreview" type="text" :disabled="!dataLoaded" class="normal_a" @click="previewUploaded">预览</el-button>
+        <el-form class="detail-form" label-position="right" label-width="100px">
+            <el-form-item label="数据名称：">
+                <span class="detail-right-label">{{detail.name}}</span>
+            </el-form-item>
+            <el-form-item label="数据源格式：">
+                <span class="detail-right-label">{{detail.inputType}}</span>
+            </el-form-item>
+            <el-form-item label="数据源文件：">
+                <span class="detail-right-label">{{detail.uploadDataName}}</span>
+            </el-form-item>
+            <el-form-item v-if="targetSheetShow" label="目标表：">
+                <span class="detail-right-label">{{detail.targetSheet | targetSheetFilter}}</span>
+            </el-form-item>
+            <el-form-item v-if="encodeShow" label="编码格式：">
+                <span class="detail-right-label">{{detail.encode}}</span>
+            </el-form-item>
+            <el-form-item v-if="symbolShow" label="分隔符：">
+                <span class="detail-right-label">{{detail.splitSymbols}}</span>
+            </el-form-item>
+            <el-form-item v-if="topRowIsTitleShow" label="首行标题行：">
+                <span class="detail-right-label">{{detail.topRowIsTitle}}</span>
+            </el-form-item>
+            <el-form-item label="数据源预览：">
                 <el-input v-if="textIsOnPreview" type="textarea" v-model="previewTxt" :rows="10" resize="none" readonly></el-input>
                 <el-table :data="previewTableData" v-if="tableIsOnPreview" :height="280" :border="true">
                     <el-table-column v-for="(head,index) in previewTableHeaders" :key="index" :prop="head" :label="head">
                     </el-table-column>
                 </el-table>
-            </div>
-        </div>
-        <div class="clearfix mgt15">
+            </el-form-item>
+        </el-form>
+        <div class="toolbar">
             <el-button v-if="$route.name=='data-source-config-create'" @click="createDataSource" type="primary" size="small">创建</el-button>
             <el-button v-if="$route.name=='data-source-config-edit'" @click="saveDataSource" type="primary" size="small">保存</el-button>
             <el-button @click="goBack" size="small">取消</el-button>
         </div>
-        <el-dialog title="设置分隔符" :visible.sync="symbolsModalVisible" size="tiny" :modal="true" :modal-append-to-body="false">
-            <p>请设置您的数据中包含的分隔符。</p>
-            <el-form label-width="100px">
-                <el-form-item class="modal-left-part" label="分隔符：">
-                    <el-checkbox-group v-model="symbolsList">
-                        <el-checkbox label="制表符"></el-checkbox>
-                        <el-checkbox label="分号"></el-checkbox>
-                        <el-checkbox label="逗号"></el-checkbox>
-                        <el-checkbox @change="spaceSymbolChange" :checked="spaceSymbolChecked" label="空格"></el-checkbox>
-                        <el-checkbox label="其它">
-                            <span>其它：
-                                <el-input v-model="otherSymbol" size="small"></el-input>
-                            </span>
-                        </el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
-                <el-form-item class="modal-right-part">
-                    <el-checkbox v-model="regardAsSingle" label="连续分隔符视为单个处理"></el-checkbox>
-                </el-form-item>
-                <el-form-item class="modal-right-part">
-                    <span>文本限定符:</span>
-                    <el-select v-model="textQualifier" class="input" size="small">
-                        <el-option v-for="item in textQualifierOptions" :key="item.value" :label="item.label" :value="item.value">
-                        </el-option>
-                    </el-select>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="symbolsModalVisible = false">取 消</el-button>
-                <el-button type="primary" :disabled="validateSymbolEmpty()" @click.native.prevent="symbolsModalConfirm">确 定</el-button>
-            </span>
-        </el-dialog>
     </div>
 </template>
 <script>
@@ -144,7 +72,8 @@ export default {
                 targetSheet: 0,
                 splitSymbols: '',
                 topRowIsTitle: true
-            }
+            },
+            detail:{}
         }
     },
     computed: {
@@ -169,71 +98,51 @@ export default {
         goBack() {
             this.$router.go(-1)
         }
+    },
+      filters: {
+
+    targetSheetFilter:function(value){
+
     }
+  }
 }
 </script>
 
 <style lang="less">
-.lightFont {
-    color: gray;
-}
-
-.modal-left-part {
-    float: left;
-    width: 200px;
-}
-
-.modal-right-part {
-    float: right;
-    width: 200px;
-    margin-right: 100px;
-    margin-top: 20px;
-}
-
-.el-checkbox-group {
-    padding-top: 20px;
-    margin-left: -15px;
-}
-
-.el-dialog__footer {
-    border-top: 1px solid #c4c4c4;
-    overflow: hidden;
-    margin-top: 200px;
-}
-
-.normal_a {
-    text-decoration: underline;
-    margin-top: -10px;
-}
-
-.uploading-progress {
-    position: absolute;
-    left: 370px;
-    top: -10px;
-}
-
 .right-content {
     .breadcrumb {
         font-size: 16px;
         line-height: inherit;
     }
+    .detail-form {
+        & .el-form-item {
+            margin: 0;
+        }
+    }
     .detail-left-label {
         height: 36px;
         line-height: 36px;
         float: left;
-        width: 100px;
+        width: 80px;
     }
-    .detail-right-content-box {
-        float: left;
-        width: calc(~"100% - 100px");
-        .input {
-            width: 250px;
-        }
-        .radio:not(:first-child) {
-            margin-right: 20px;
-            margin-top: 15px;
-            margin-left: 0;
-            width: 100px;
+    .detail-right-label {
+        width: auto;
+        color: grey;
+    }
+    .toolbar {
+        position: absolute;
+        right: 40px;
+        top: 40px;
+        margin-top: 32px;
+        button {
+            margin: 0;
+            &:after {
+                content: " |";
+                display: inline;
+            }
+            &:last-child:after {
+                display: none;
+            }
         }
     }
 }
